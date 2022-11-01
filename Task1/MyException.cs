5f8f7ac6596fbe6befddab9967fc1cca92c233af
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-11-01 21:27:22 +0300

    Добавьте файлы проекта.

---
+++ Task1/MyException.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        {
+            HelpLink = "www.ya.ru";
+        }
+
+        public MyException(string message)
+            : base(message)
+        {
+            HelpLink = "www.ya.ru";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = {
+                new MyException("Отсутствует доступ к сайту"),
+                new DriveNotFoundException(),
+                new DirectoryNotFoundException(),
+                new FormatException(),
+                new UriFormatException()
+            };
+
+            try
+            {
+                throw exceptions[0];
+            }
+
+            catch (Exception ex)
+            {
+                if (exceptions.Contains(ex))
+                    Console.WriteLine(ex.Message);
+                else
+                    Console.WriteLine("Неизвестное исключение");
+            }
+        }
+    }
+}
